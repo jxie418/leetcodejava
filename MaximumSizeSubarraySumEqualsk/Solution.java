0640a7956acb8331430807c1505d29ce2b8a959d
James Xie <jxie418@gmail.com>
2016-02-18 20:07:39 -0800

    Add Maximum Size Subarray sum equals k

--- MaximumSizeSubarraySumEqualsk/Solution.java
+++ MaximumSizeSubarraySumEqualsk/Solution.java
@@ -1,3 +1,15 @@
+/*
+
+medium
+tag: hashmap, preSum
+similar: continuous array sum
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one, return 0 instead.
+
+eg : Given nums = [1, -1, 5, -2, 3], k = 3, return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
+参考的discuss里面的1-pass解法, link
+思路和我的类似, 也是在hashmap里面找sum-k的index, 但就是写得好. 我修改了一下, 即使用virtual preSum: 0存在-1的位置. 这在array, list, graph里面经常使用, 统一解决边界问题.
+代码如下, 注意sum只存1次, 因为是找max length, 所以是最远距离, 所以保存第一次出现的位置.
+*/
 import java.util.*;
 public class Solution {
   public int maxSubArrayLen(int[] nums, int k) {
